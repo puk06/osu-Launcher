0c3f0b98667b1ad160999e2f468f4831770c3aac
Puk06 <86549420+puk06@users.noreply.github.com>
2025-02-01 12:00:39 +0900

    バージョン変更

--- Forms/Main.cs
+++ Forms/Main.cs
@@ -17,7 +17,7 @@ namespace osu_launcher.Forms
 {
     public partial class Main : Form
     {
-        public const string CurrentVersion = "v1.0.4-Release";
+        public const string CurrentVersion = "v1.0.5-Release";
 
         // Data Values
         public JObject Data;
